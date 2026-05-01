8014658dfc29086f53b01c93bec3e84d2ce25396
ruhul2003 <ruhul941020@gmail.com>
2026-05-01 12:13:29 +0600

    Project initialized

---
+++ src/app/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h1>It is a navbar</h1>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loginPage = () => {
+    return (
+        <div>
+            <h1>This is a login page</h1>
+        </div>
+    );
+};
+
+export default loginPage;
